4916bdfaeae59649491daa04fc052d39dc1e30b2
unknown <1278800400@qq.com>
2019-04-17 20:03:28 +0800

    优化

--- static/config.js
+++ static/config.js
@@ -1,6 +1,8 @@
+const api = "https://api.9knx.com";
 window.g = {
   AXIOS_TIMEOUT: 10000,
   ApiUrl: 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=', // 配置服务器地址,
+  distributon:api+"/api/member",
   ParentPage: {
     LoginApi:'http://md.9knx.com:9099/sale/login',
     DeteailsApi:'http://www.phonegap100.com/appapi.php?a=getPortalArticle&aid=',
