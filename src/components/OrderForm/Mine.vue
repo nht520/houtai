f6560e510a92002ecfb21847dd6b116caaf746c9
nahetian <wlgou6363@qq.com>
2019-04-20 17:04:26 +0800

    订货订单

--- src/components/OrderForm/Mine.vue
+++ src/components/OrderForm/Mine.vue
@@ -1,29 +1,193 @@
 <template>
-  <div id="mine">
+  <div id="Mine">
     <Header :header="title"></Header>
+    <!--  头部-->
+    <div class="list">
+      <!--引用表格-->
+      <el-table
+        :data="list"
+        ref="multipleTable"
+        tooltip-effect="dark"
+        style="width: 100%"
+        row-key="reserve-selection"
+        @selection-change="selectionRowsChange"
+      >
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column
+          label="订货人姓名">
+          <template slot-scope="scope">
+            <span >{{ scope.row.memberEntity.memberName }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="memberGender"
+          label="订货人电话"
+        >
+          <template slot-scope="scope">
+            <span >{{ scope.row.memberEntity.memberPhone }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="buyTime"
+          label="订货时间">
+        </el-table-column>
+        <el-table-column
+          prop="mealEntity"
+          label="所订套餐">
+          <template slot-scope="scope">
+            <span >{{ scope.row.mealEntity.mealName }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="套餐数量">
+          <template slot-scope="scope">
+            <span >{{ scope.row.mealEntity.mealNum }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="套餐价格">
+          <template slot-scope="scope">
+            <span >{{ scope.row.mealEntity.mealPrice }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+<!--            <el-button-->
+<!--              size="mini"-->
+<!--              @click="compile">编辑</el-button>-->
+            <el-button
+              size="mini"
+              type="danger"
+              @click.native.prevent="deleteRow(scope.$index, list)">删除</el-button>
+            <el-button
+              size="mini"
+              type="success"
+              @click="examine">查看</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 引用分业-->
+      <div id="Navpages">
+        <el-row>
+          <el-col :span="12" class="left">
+            <el-button @click="toggleSelect(list)" size="mini">全选/反选</el-button>
+            <el-button type="danger" size="mini" @click="qxDete" >删除</el-button>
+          </el-col>
+          <!-- 分业-->
+          <el-col :span="12">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="current"
+              :page-sizes="[10, 20]"
+              :page-size=size
+              layout="total, sizes, prev, pager, next, jumper"
+              :total=total>
+            </el-pagination>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
   import Header from "../Header/Header";
+  import Axios from "axios";
   export default {
-    name: "Distribution",
+    name: "Mine",
     components: {Header},
     data(){
       return{
-        title:"订单管理"
+        title:"订单管理",
+        current: 1,
+        list: [],
+        total:0,
+        size:0,
+        present:1,
+        number:"10",
       }
     },
     methods:{
-
+      //编辑
+      compile (){
+        console.log("编辑")
+      },
+      //查看详情
+      examine(){
+        this.$router.push({path:'/Details'});
+        console.log(this.title)
+      },
+      //全选
+      toggleSelect(rows) {
+        console.log(rows);
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      //获取全选的key
+      selectionRowsChange(val){
+        console.log(val);
+      },
+      //删除当前一行
+      deleteRow(index, rows) {
+        rows.splice(index, 1);
+      },
+      //删除选中数据
+      qxDete(){
+        console.log("删除选中数据");
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+        this.number=val;
+        this.distList();
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+        this.present=val;
+        this.distList();
+      },
+      distList(){
+        // const api = "https://api.9knx.com/api/member?current="+this.present+"&size="+this.number;
+        const api = window.g.stockLog;
+        const param ={
+              params:{
+                current:this.present,
+                size:this.number
+              }
+        };
+        Axios.get(api,param).then((res)=>{
+          console.log(res);
+          this.list=res.data.records;
+          this.total=res.data.total;
+          this.size=res.data.size;
+          this.pages=res.data.pages;
+          this.current=res.data.current;
+        },(err)=>{
+          console.log(err)
+        })
+      }
     },
     mounted() {
-
+      this.distList();
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  #mine
-   width 100%
+  .left
+    text-align left
+    padding-left  1%
+  #Mine
+    width 100%
+  #Navpages
+    text-align right
+    padding 1% 1% 1% 0%
+    background #FFFFFF
 </style>
--- static/config.js
+++ static/config.js
@@ -1,9 +1,38 @@
+// const besurl = "http://192.168.3.45:8899";
+const besurl = "http://wx.bomao.xyz:8080/distributor";
 window.g = {
   AXIOS_TIMEOUT: 10000,
-  ApiUrl: 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=', // 配置服务器地址,
+  login:besurl+"/distributor/member/login",
+  findById:besurl+"/distributor/member/findById",
+  register:besurl+'/distributor/member/updatePass',
+  mcMembers:besurl+'/distributor/mcMembers/findById',
+  //我的库存
+  getBoxNum:besurl+"/distributor/stock/getBoxNum",
+  //订货订单
+  stockLog:besurl+'/distributor/stockLog',
+  //库存
+  stock:besurl+'/distributor/stock',
+  //  套餐
+  meal:besurl+"/distributor/meal",
+  mealDts:besurl+"/distributor/meal/findById?id=",
+  //自己发货订单
+  indent:besurl+"/distributor/order",
+  update:besurl+"/distributor/order/update",
+  //地址
+  // 省
+  getProvince:besurl+"/distributor/systemAddress/getProvince",
+  //市province
+  getCity:besurl+"/distributor/systemAddress/getCity",
+  //区city
+  getCounty:besurl+"/distributor/systemAddress/getCounty",
+  //添加地址
+  addressadd:besurl+"/distributor/address",
+//  修改地址
+  adsupdate:besurl+"/distributor/address/update",
+  //
   ParentPage: {
-    LoginApi:'http://md.9knx.com:9099/sale/login',
-    DeteailsApi:'http://www.phonegap100.com/appapi.php?a=getPortalArticle&aid=',
-    News:"http://v.juhe.cn/toutiao/index"
+    // http://wx.bomao.xyz:8080/distributor
+    // ApiUrl: 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=', // 配置服务器地址,
+    // DeteailsApi:'http://www.phonegap100.com/appapi.php?a=getPortalArticle&aid=',
   },
-}
+};
